97e415e6a408476ad4b7a1064d107e5fd0e9f185
George Sigoiu <georgesigoiu1@gmail.com>
2022-11-23 21:23:56 +0200

    add MessageQueueReceiver for getting the info from rabbitmq queue

--- src/main/java/org/sigoiugeorge/energy/service/impl/MessageQueueReceiver.java
+++ src/main/java/org/sigoiugeorge/energy/service/impl/MessageQueueReceiver.java
@@ -1,37 +1,35 @@
 package org.sigoiugeorge.energy.service.impl;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.minidev.json.JSONObject;
+import org.sigoiugeorge.energy.model.MeteringDevice;
 import org.sigoiugeorge.energy.service.api.EnergyConsumptionService;
+import org.sigoiugeorge.energy.service.api.MeteringDeviceService;
 import org.sigoiugeorge.energy.utils.EnergyConsumptionResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.sigoiugeorge.energy.websocket.TextMessageDTO;
+import org.sigoiugeorge.energy.websocket.WebSocketTextController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class MessageQueueReceiver {
     @Value("${rabbitmq.queue.name}")
     private String queueName;
     @Value("${rabbitmq.connection.uri}")
     private String uri;
 
-    private EnergyConsumptionService energyConsumptionService;
-
-    @Autowired
-    private void setEnergyConsumptionService(EnergyConsumptionService service) {
-        energyConsumptionService = service;
-    }
-
+    private final EnergyConsumptionService energyConsumptionService;
+    private final MeteringDeviceService deviceService;
+    private final WebSocketTextController ws;
 
     public void start() throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -51,11 +49,22 @@ public class MessageQueueReceiver {
                     .readerFor(EnergyConsumptionResponse.class)
                     .readValue(message);
             energyConsumptionService.addEnergyConsumption(readObject);
+            Boolean valueExceeded = deviceService.deviceExceededMaxConsumption(readObject.getDeviceId());
+            if (valueExceeded) {
+                JSONObject json = new JSONObject();
+                MeteringDevice device = deviceService.get(readObject.getDeviceId());
+                String address = device.getAddress();
+                Integer maxHourlyEnergyConsumption = device.getMaxHourlyEnergyConsumption();
+                json.put("address", address);
+                json.put("max_consumption", maxHourlyEnergyConsumption);
+                TextMessageDTO textMessageDTO = new TextMessageDTO(json.toString());
+                System.out.println("Message to send next: " + textMessageDTO);
+                ws.sendMessage(textMessageDTO);
+            }
         };
         channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
         });
     }
-
 
 
 }
